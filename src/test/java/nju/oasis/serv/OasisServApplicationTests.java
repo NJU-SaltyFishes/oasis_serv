d8e6ad5e43d0bbca5342e34bb261e219b614a93d
reverse-mA <anhao0429@163.com>
2020-11-16 17:43:00 +0800

    添加Author测试

--- src/test/java/nju/oasis/serv/OasisServApplicationTests.java
+++ src/test/java/nju/oasis/serv/OasisServApplicationTests.java
@@ -2,9 +2,11 @@ package nju.oasis.serv;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@WebAppConfiguration
 @SpringBootTest
-class OasisServApplicationTests {
+public class OasisServApplicationTests {
 
 	@Test
 	void contextLoads() {
