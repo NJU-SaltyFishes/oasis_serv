8604eddeea072a228a349b6506b0bcbb0a45510c
reverse-mA <anhao0429@163.com>
2020-11-16 21:03:05 +0800

    feat:修改查找合作最密切作者和引用次数最多论文业务

--- src/main/java/nju/oasis/serv/serviceImpl/AuthorServiceImpl.java
+++ src/main/java/nju/oasis/serv/serviceImpl/AuthorServiceImpl.java
@@ -1,6 +1,7 @@
 package nju.oasis.serv.serviceImpl;
 
 import lombok.extern.slf4j.Slf4j;
+import nju.oasis.serv.dao.AuthorDAO;
 import nju.oasis.serv.domain.Article;
 import nju.oasis.serv.domain.CoAuthor;
 import nju.oasis.serv.provider.CoAuthorProvider;
@@ -13,6 +14,7 @@ import nju.oasis.serv.vo.ResultCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,14 +24,8 @@ import java.util.concurrent.ConcurrentHashMap;
 @Slf4j
 public class AuthorServiceImpl implements AuthorService {
 
-    @Autowired
-    Pipeline pipeline;
-
-    @Autowired
-    CoAuthorProvider coAuthorProvider;
-
-    @Autowired
-    MostCitedArticleProvider mostCitedArticleProvider;
+    @Resource
+    AuthorDAO authorDAO;
 
     @Override
     public ResponseVO findById(AuthorRequestForm authorRequestForm){
@@ -47,8 +43,13 @@ public class AuthorServiceImpl implements AuthorService {
         }
         Map<String, Object> result = new HashMap<>();
         //新建pipeline，并发跑查找引用最多论文和查找合作最多作者任务
-        pipeline.setParamToContextData("authorId",authorRequestForm.getAuthorId());
-        pipeline.setParamToContextData("articleIds",authorRequestForm.getArticleIds());
+        Pipeline pipeline = new Pipeline();
+
+        MostCitedArticleProvider mostCitedArticleProvider = new
+                MostCitedArticleProvider(authorDAO,authorRequestForm.getArticleIds());
+        CoAuthorProvider coAuthorProvider = new CoAuthorProvider(authorDAO,
+                authorRequestForm.getAuthorId(),authorRequestForm.getArticleIds());
+
         pipeline.addProviderAsGroup(mostCitedArticleProvider,coAuthorProvider);
         pipeline.doPipeline();
         ConcurrentHashMap<String, Object> concurrentHashMap = pipeline.getContextData();
